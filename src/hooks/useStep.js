e5b86bf644aa9e57ddb6211481b0cb97613fa293
DictumMortuum <theio.vrefos@gmail.com>
2022-07-15 08:42:55 +0300

    [hooks] best price filtering has to come after stock is filtered

--- src/hooks/useStep.js
+++ src/hooks/useStep.js
@@ -40,9 +40,9 @@ export const useStep = f => {
   const [min, max] = price_range;
   const page_filtered = f !== undefined ? f(prices) : prices
   const range_filtered = enable_price_filter ? page_filtered.filter(d => d.price < max && d.price > min) : page_filtered;
-  const best_filtered = enable_best_price ? bestPriceFilter(range_filtered) : range_filtered;
-  const stock_filtered = best_filtered.filter(stockFilter(stock))
-  const store_filtered = stock_filtered.filter(storeFilter(store))
+  const stock_filtered = range_filtered.filter(stockFilter(stock));
+  const best_filtered = enable_best_price ? bestPriceFilter(stock_filtered) : stock_filtered;
+  const store_filtered = best_filtered.filter(storeFilter(store));
 
   return {
     page_filtered,

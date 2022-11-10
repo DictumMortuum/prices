b046d0c62f1287133f35ec235b7572e84b4f7d5b
DictumMortuum <theio.vrefos@gmail.com>
2022-11-10 13:34:07 +0200

    Updates

---
+++ precache-manifest.f96fe123511dfef26ec20144645423b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e871f3f46ec1bf86ba89",
+    "url": "/static/css/main.72e772db.chunk.css"
+  },
+  {
+    "revision": "e871f3f46ec1bf86ba89",
+    "url": "/static/js/main.e24c67c0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e2c296d2351668aae1d8",
+    "url": "/static/js/2.809b32ad.chunk.js"
+  },
+  {
+    "revision": "dd87c6d4f511ae172bf681852532b61c",
+    "url": "/static/media/cartoff.dd87c6d4.svg"
+  },
+  {
+    "revision": "cbc2e3d2b67c66155532072fb6e230d6",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5c56868f66c92c87d31a58ac4d0aa437.js"
+  "/precache-manifest.f96fe123511dfef26ec20144645423b1.js"
 );
 
 workbox.clientsClaim();
